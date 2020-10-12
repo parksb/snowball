2be36a56f14b6da5596abf201fe99baf20831017
parksb <parkgds@gmail.com>
2020-10-12 21:18:11 +0900

    v1.3.0

--- dist/Storage.d.ts
+++ dist/Storage.d.ts
@@ -1,5 +1,5 @@
 declare class Storage {
-    static get(key: string): string;
+    static get(key: string): string | null;
     static set(key: string, value: string): void;
 }
 export default Storage;
--- dist/Storage.js
+++ dist/Storage.js
@@ -2,11 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class Storage {
     static get(key) {
-        const found = localStorage.getItem(key);
-        if (!found) {
-            throw new Error('Failed to load');
-        }
-        return found;
+        return localStorage.getItem(key);
     }
     static set(key, value) {
         localStorage.setItem(key, value);
